f63b16b6f264f0f5faaccf142c87be97f0a8aa69
vladimir morozov <vmorozov@ot.ru>
2020-08-04 02:46:36 +0300

    убрал прозрачность для выбор темы

--- eva/src/components/themeSettings.vue
+++ eva/src/components/themeSettings.vue
@@ -5,7 +5,7 @@
     class="theme-setting"
     right
     :color="colorFrom.text"
-    :style="{background: addColorOpacity(colorFrom.backElement, 60)}"
+    :style="{background: colorFrom.backElement}"
   >
     <div class="theme-btn">
       <v-btn 
@@ -71,7 +71,7 @@ export default {
   computed: {
     active: function() { 
       return this.showFrom 
-    } 
+    },
   },  
   watch: {
     paleteFrom: function (palete) {
@@ -101,19 +101,6 @@ export default {
       });
       this.$store.commit('setThemeBack', JSON.stringify({setting: settings}));
     },
-    addColorOpacity(hexColor, opacity){
-      const hex = hexColor.replace('#', '');
-    
-      if (hex.length === 3) {
-        hex += hex
-      }
-      
-      const r = parseInt(hex.substring(0, 2), 16);
-      const g = parseInt(hex.substring(2, 4), 16);
-      const b = parseInt(hex.substring(4, 6), 16);
-
-      return `rgba(${r},${g},${b},${opacity / 100})`;
-    }
   },
 }
 
