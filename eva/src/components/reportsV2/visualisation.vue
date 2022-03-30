14d8215d828cd527393cf19a94b553b1976d21c6
nIKaaapav <nikapavlenko20.np@gmail.com>
2022-03-30 16:08:21 +0200

    bugfix: Data Exploration 2.0 Some visualizations are not displayed immediately after selection

--- eva/src/components/reportsV2/visualisation.vue
+++ eva/src/components/reportsV2/visualisation.vue
@@ -2,7 +2,10 @@
   <div
     ref="vis"
     class="visualisation"
-    :style="{ background: theme.$main_bg, color: theme.$main_text }"
+    :style="{
+      background: theme.$main_bg,
+      color: theme.$main_text,
+      minHeight: activeElem ==='bush' ? '400px' : undefined }"
   >
     <div class="header-settings">
       <v-menu
@@ -268,7 +271,6 @@ export default {
 
 <style lang="scss">
 .visualisation {
-  min-height: 250px;
   flex-grow: 1;
   position: relative;
   width: 100%;
