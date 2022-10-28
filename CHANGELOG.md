27d0b053a8165472518b1fb6198b49774b5c6726
Vitaliy Kalinovskiy <vkalinovskiy@isgneuro.com>
2022-10-28 11:22:53 +0200

    Fixed a bug with the presence of a dot in the name of the data column

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 ### Fixed
+- Fixed a bug with the presence of a dot in the name of the data column
 
 ## [1.109.0]
 
--- eva/src/store/storeRest.js
+++ eva/src/store/storeRest.js
@@ -202,11 +202,14 @@ export default {
                     });
 
                     if (shema != null && shema !== '') {
-                      const keys = shema.match(/`[^`.]+`/g).map((item) => item.replace(/`/g, ''));
-                      const values = shema
-                        .replace(/`[^`.]+`/g, '')
-                        .replace(/\s*/g, '')
-                        .split(',');
+                      const keys = [];
+                      const values = [];
+                      shema.split(',').forEach((str) => {
+                        const [, key, value] = str.match(/^`(.*)`\s(\w+)$/);
+                        keys.push(key.replace('``', '`'));
+                        values.push(value);
+                      });
+
                       shema = {};
                       keys.forEach((item, i) => {
                         shema[item] = values[i];
